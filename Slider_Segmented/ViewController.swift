5fbaca38128e20258bd7889a79852ec5d7158250
Taalai <taalai.z01@gmail.com>
2021-05-28 09:13:23 +0600

    Initial Commit

---
+++ Slider_Segmented/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider_Segmented
+//
+//  Created by Amazing on 5/28/21.
+//  Copyright © 2021 Taalai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
